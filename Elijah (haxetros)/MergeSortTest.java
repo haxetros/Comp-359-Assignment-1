934505d55b052f8ea73ff9810773099577f8897b
haxetros <15054115+haxetros@users.noreply.github.com>
2024-09-20 04:04:06 -0700

    Remove old code from MergeSortTest.java

--- Elijah (haxetros)/MergeSortTest.java
+++ Elijah (haxetros)/MergeSortTest.java
@@ -13,9 +13,6 @@ public class MergeSortTest {
         int[] arrSorted = {1, 2, 3, 4, 5, 6, 7, 8};
         int[] expcectedSorted = {1, 2, 3, 4, 5, 6, 7, 8};
         MergeSort.extraMemoryMergeSort(arrSorted, arrSorted.length);
-        assertArrayEquals(arrSorted, arrSorted);
-        int[] expcectedSorted = {1, 2, 3, 4, 5, 6, 7, 8};
-        MergeSort.extraMemoryMergeSort(arrSorted, arrSorted.length);
         assertArrayEquals(expcectedSorted, arrSorted);
 
         int[] arrReverse = {8, 7, 6, 5, 4, 3, 2, 1};
@@ -47,9 +44,6 @@ public class MergeSortTest {
         assertArrayEquals(expected, arr);
 
         int[] arrSorted = {1, 2, 3, 4, 5, 6, 7, 8};
-        int[] expcectedSorted = {1, 2, 3, 4, 5, 6, 7, 8};
-        MergeSort.inPlaceMergeSort(arrSorted);
-        assertArrayEquals(arrSorted, arrSorted);
         int[] expcectedSorted = {1, 2, 3, 4, 5, 6, 7, 8};
         MergeSort.inPlaceMergeSort(arrSorted);
         assertArrayEquals(expcectedSorted, arrSorted);
